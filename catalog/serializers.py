4916eff2b9a9badde0dc309561b88c91464e28cb
adcc662 <adcc662@gmail.com>
2025-01-12 16:52:30 -0600

    Serializer modification to do the POST

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -4,11 +4,11 @@ from catalog.models import Category, Commerce
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = []
+        fields = ['id', 'name', 'type']
 
 class CommerceSerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
-
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), source='category')
     class Meta:
         model = Commerce
-        fields = ['id','merchant_name', 'merchant_logo', 'category']
+        fields = ['id','merchant_name', 'merchant_logo', 'category', 'category_id']
