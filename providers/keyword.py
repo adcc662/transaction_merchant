ee7de32e838dfb469ac8226e50b53e91269e116f
adcc662 <adcc662@gmail.com>
2025-01-12 16:58:01 -0600

    Improving the logic of the keyword and transaction

--- providers/keyword.py
+++ providers/keyword.py
@@ -1,4 +1,4 @@
 from transaction.models import Keyword
 
-def get_keywords_description(description_words):
+def get_keywords_by_description_words(description_words):
     return Keyword.objects.filter(keyword__in=description_words).select_related('merchant__category')
--- providers/transaction.py
+++ providers/transaction.py
@@ -6,19 +6,21 @@ def create_transactions(transactions):
     created_transactions = []
     for transaction_data in transactions:
         try:
+            # Extracting fields with default to None if not provided
             category_id = transaction_data.get('category_id')
             merchant_id = transaction_data.get('merchant_id')
 
             transaction = Transaction.objects.create(
-                id=transaction_data.get['id'],
-                description=transaction_data.get['description'],
-                amount=transaction_data.get['amount'],
-                date=transaction_data.get['date'],
+                id=transaction_data['id'],
+                description=transaction_data['description'],
+                amount=transaction_data['amount'],
+                date=transaction_data['date'],
                 category_id=category_id,
                 merchant_id=merchant_id
             )
             created_transactions.append(transaction)
         except Exception as e:
             print(f"Error creating transaction: {e}")
+
 
     return created_transactions
