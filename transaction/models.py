0ec23cba0790cdd171d77e50a5d6991f49601324
adcc662 <adcc662@gmail.com>
2025-01-12 16:59:53 -0600

    Rename field commerce for merchant

--- transaction/models.py
+++ transaction/models.py
@@ -19,8 +19,8 @@ class Transaction(UUIDModel):
     description = models.TextField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
-    commerce = models.ForeignKey(Commerce, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    merchant = models.ForeignKey(Commerce, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.description
